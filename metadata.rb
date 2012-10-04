40c1c603edcbb99bb6a1f048ade0c21b42e25410
David Laing <david@davidlaing.com>
2012-10-04 16:23:44 +0000

    Step1 - declare support for windows

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ license          "Apache 2.0"
 description      "Installs/Configures Splunk Storm forwarders"
 long_description IO.read(File.join(File.dirname(__FILE__), 'readme.md'))
 version          "0.0.1"
-%w{redhat centos fedora debian ubuntu}.each do |os|
+%w{redhat centos fedora debian ubuntu windows}.each do |os|
   supports os
 end
